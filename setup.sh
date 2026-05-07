f363fda7dcbeb4366751a2a77ec8a7c98e9d2d96
ikeyan <kik@nvstl.com>
2026-05-07 23:25:04 +0900

    Default環境のセットアップスクリプト

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+touch /root/setup.log
+git clone --depth=1 https://github.com/ikeyan/agent-files.git /root/agent-files > /root/setup.log 2>&1
+mv /root/.claude/skills/* /root/agent-files/skills/
+rmdir /root/.claude/skills
+ln -s /root/agent-files/skills /root/.claude/skills
+
+if [[ -f .setup-sandbox.sh ]]; then
+  /bin/bash .setup-sandbox.sh
+elif [[ -f package-lock.json ]]; then
+  npm ci
+elif [[ -f pnpm-lock.yaml ]]; then
+  pnpm install --frozen-lockfile
+elif [[ -f yarn.lock ]]; then
+  yarn install --frozen-lockfile
+elif [[ -f bun.lock ]]; then
+  bun install --frozen-lockfile
+fi
